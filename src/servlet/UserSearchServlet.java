55d5eef65e9e80b5715a2a2009e5b7f0c8f7ba32
ogura-shotaro <ogura-shotaro-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 18:16:06 +0900

    UserSearch関連のファイルを作成

--- src/servlet/UserSearchServlet.java
+++ src/servlet/UserSearchServlet.java
@@ -16,11 +16,28 @@ import javax.servlet.http.HttpServletResponse;
 public class UserSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
+	String follow = "";
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		if (follow.equals("0")) {
+			//followテーブルにレコードを追加する処理！
+
+		} else if (follow.equals("1")) {
+			//followテーブルからレコードを削除する処理！
+
+		}
+		if (false) {
+			//()の中でfollowsテーブルのuser_idに自分のidの入ったレコードをDAOからもらう。
+			//で、それをリクエストスコープでフォワード先に送る
+		}
+
+		//usersテーブルのidにfollowsテーブルのuser_idを外部結合して、
+		//users.user_id,users.pic,follows.follow_user_idの項目で取り出し
+		//それをリクエストスコープでフォワード先に送る
+
 
 		// ユーザー検索・一覧ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user.jsp");
