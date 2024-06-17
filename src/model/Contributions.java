63690dc27b7711932a46d1f93b6d657c8942f3ba
ogura-shotaro <ogura-shotaro-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 18:05:11 +0900

    Contributionテーブルのmodelとdaoを少し書いた

--- src/model/Contributions.java
+++ src/model/Contributions.java
@@ -1,5 +1,92 @@
 package model;
 
-public class Contributions {
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Contributions implements Serializable {
+	//フィールド
+	private int id;					// 投稿ID
+	private String user_id;			// ユーザーID
+	private int rikishi_id;			// 力士ID
+	private String pic_movie;		/* 写真・動画 */
+	private String text;			// 投稿テキスト
+	private Timestamp created_at;	// 投稿日時
+	private Timestamp updated_at;	// 更新日時
+
+
+	//全部入りのコンストラクタ(検索に入るとき)
+	public Contributions(int id, String user_id, int rikishi_id, String pic_movie, String text, Timestamp created_at,
+			Timestamp updated_at) {
+		this.id = id;
+		this.user_id = user_id;
+		this.rikishi_id = rikishi_id;
+		this.pic_movie = pic_movie;
+		this.text = text;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+
+	//デフォルトコンストラクタ
+	public Contributions() {
+
+	}
+
+
+	//getter,setter
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public int getRikishi_id() {
+		return rikishi_id;
+	}
+
+	public void setRikishi_id(int rikishi_id) {
+		this.rikishi_id = rikishi_id;
+	}
+
+	public String getPic_movie() {
+		return pic_movie;
+	}
+
+	public void setPic_movie(String pic_movie) {
+		this.pic_movie = pic_movie;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+
+	public Timestamp getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Timestamp updated_at) {
+		this.updated_at = updated_at;
+	}
 
 }
