e0bf3b2f710d8730a21ef8e8c3c82638e4d8b0ec
ogura-shotaro <ogura-shotaro-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-20 10:51:10 +0900

    ContributionsDaoの最初のメソッドのテストまで

--- src/dao/ContributionsDao.java
+++ src/dao/ContributionsDao.java
@@ -45,11 +45,7 @@ public class ContributionsDao {
 				Contributions record = new Contributions(
 				rs.getInt("id"),
 				rs.getString("user_id"),
-				rs.getInt("rikishi_id"),
-				rs.getString("pic_movie"),
-				rs.getString("text"),
-				rs.getTimestamp("created_at"),
-				rs.getTimestamp("updated_at")
+				rs.getString("pic_movie")
 				);
 				contributionsList.add(record);
 			}
