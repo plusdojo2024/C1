6c8880b680c52f517a1c8e8e3c42f41d4dc44209
plusdojo2024-sugimoto <sugimoto-tomoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-23 23:41:14 +0900

    待機部屋初期表示完了

--- src/servlet/StandByServlet.java
+++ src/servlet/StandByServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.RikishiesDao;
+import model.Rikishies;
+import model.Users;
 
 /**
  * Servlet implementation class StandByServlet
@@ -19,13 +25,25 @@ public class StandByServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		/*HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/simpleBC/LoginServlet");
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/C1/LoginServlet");
 			return;
-		}*/
+		}
+		//ゲットパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		Users User = (Users) session.getAttribute("user_id");
+		String User_id = User.getUser_id();
+
+		// 初期表示のための全件検索を行う
+		RikishiesDao rikishies = new RikishiesDao();
+		List<Rikishies> rikishiesList = rikishies.select_StandBy(new Rikishies(User_id));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("rikishiesList", rikishiesList);
 
 		// 待機部屋にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/standBy.jsp");
