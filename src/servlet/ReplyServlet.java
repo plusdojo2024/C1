18fe8ca5a96f1184c194255cc247a1402c919f43
ogura-shotaro <ogura-shotaro-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-24 14:07:56 +0900

    ReplyServlet関係を一部修正

--- src/servlet/ReplyServlet.java
+++ src/servlet/ReplyServlet.java
@@ -40,27 +40,26 @@ public class ReplyServlet extends HttpServlet {
 		String User_id = User.getUser_id();
 
 		//クエリパラメータを取得
-        String star = request.getParameter("star");
-		int id = Integer.parseInt(request.getParameter("id"));
 		String judge = request.getParameter("judge");
+		int id = Integer.parseInt(request.getParameter("id"));
 
 		//処理
 		ContributionsDao contributions = new ContributionsDao();
-		if (judge==null) {}
-		else if (judge.equals("1")){
-		if (star==null) {
-			//starsテーブルにレコードを追加する処理
-			contributions.insertStar(new Contributions(id, User_id));
+		if (judge==null) {
+	        String star = request.getParameter("star");
+			if (star==null) {
+				//starsテーブルにレコードを追加する処理
+				contributions.insertStar(new Contributions(id, User_id));
 
-		} else if (star.equals(User_id)) {
-			//starsテーブルからレコードを削除する処理
-			contributions.delete(new Contributions(id, User_id));
+			} else if (star.equals(User_id)) {
+				//starsテーブルからレコードを削除する処理
+				contributions.delete(new Contributions(id, User_id));
 
-		}
+			}
 		}
 
 		// 初期表示の処理
-		List<Contributions> contributionsList = contributions.selectReplyContribution(new Contributions(id));
+		List<Contributions> contributionsList = contributions.selectReplyContribution(new Contributions(id, User_id));
 
 
 		// 検索結果をリクエストスコープに格納する
