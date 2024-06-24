df796a08cb140c1723e5d4ad660775926cee45dc
plusdojo2024-sugimoto <sugimoto-tomoka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-24 10:57:17 +0900

    stars初期表示

--- src/servlet/StarsServlet.java
+++ src/servlet/StarsServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.StarsDao;
+import model.Stars;
+import model.Users;
 
 /**
  * Servlet implementation class StarsServlet
@@ -21,11 +27,26 @@ public class StarsServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		/*HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/simpleBC/LoginServlet");
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/C1/LoginServlet");
 			return;
-		}*/
+		}
+
+		//リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		Users User = (Users)session.getAttribute("user_id");
+		String User_id = User.getUser_id();
+
+
+
+		// 検索処理を行う
+		StarsDao stars = new StarsDao();
+		List<Stars> starsList = stars.select(new Stars(User_id));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("starsList", starsList);
+
 
 		// 白星一覧にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/stars.jsp");
